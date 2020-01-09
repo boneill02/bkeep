926515913a35b19ecf3ea7a717156b17eabbe487
Ben O'Neill <ben@benoneill.xyz>
2020-01-09 12:59:36 -0500

    Add Open Library API scrpit

---
+++ testing/openlibrarytest.py
@@ -0,0 +1,22 @@
+import argparse
+import requests
+
+def get_info(isbn):
+    return requests.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + isbn + '&jscmd=data&format=json').json()
+
+def print_info(json_result, isbn):
+    print(json_result)
+    title = json_result['ISBN:' + isbn]['title']
+    author = json_result['ISBN:' + isbn]['authors'][0]['name']
+    print(title + '\t' + author + '\t\t' + isbn)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i')
+    args = parser.parse_args()
+    isbn = args.i
+    if isbn != None:
+        json_result = get_info(isbn)
+        print_info(json_result, isbn)
+    else:
+        print('Must provide ISBN')
